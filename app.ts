afa7fbfe7c3c121716eacab8dd8cd73480db8abe
vikasrohra <vikasrohra2@gmail.com>
2024-04-10 00:23:26 +0530

    Added first typescript method

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('Your code goes here...');
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+console.log(add(number1, number2));
