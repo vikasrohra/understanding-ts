a5a47d1938ef1287e53666a892acea29923a1ea8
vikasrohra <vikasrohra2@gmail.com>
2024-04-10 22:28:45 +0530

    never type

--- app.ts
+++ app.ts
@@ -7,3 +7,23 @@ let userName: string;
 if(typeof userInput === "string") {
     userName = userInput;
 }
+
+// function generateError(message: string, code: number) {
+//     throw {message: message, errorCode: code};
+// }
+
+// const result = generateError("An error occurred!", 500);
+// console.log(result);
+
+// // Output:
+// // Uncaught: {message: 'An error occurred!', errorCode: 500}
+// // This method will never return, as error is thrown that crashes our script
+// // If you hover over generateError, it shows void because never is newer but if we know it can never return then we can explicitly assign it to never
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    // while(true) {}
+}
+
+const result = generateError("An error occurred!", 500);
+console.log(result);
