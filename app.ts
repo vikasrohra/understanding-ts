c1242e6849591143aae05f8ddd602428d5a1e43e
vikasrohra <vikasrohra2@gmail.com>
2024-04-10 18:23:44 +0530

    Array declaration in TS

--- app.ts
+++ app.ts
@@ -18,3 +18,12 @@ const person2 = {
 }
 
 console.log(person2.name);
+
+
+// Array that can only have string elements
+let favActivities: string[];
+favActivities = ["Sports", "1"];
+
+// Array that can have elements of any type
+let activities: any[];
+activities = ["Sports", 1];
