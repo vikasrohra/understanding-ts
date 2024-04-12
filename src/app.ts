9afe1bfd3d68f3d24fed1df241a0468f8fd17d63
vikasrohra <vikasrohra2@gmail.com>
2024-04-12 22:03:05 +0530

    Change configuration inside tsconfig.json

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ if(typeof userInput === "string") {
 
 // function generateError(message: string, code: number) {
 //     throw {message: message, errorCode: code};
-// }
+// }    
 
 // const result = generateError("An error occurred!", 500);
 // console.log(result);
