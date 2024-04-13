9c390f6dadc4f9397959ce29092cce961e225b37
vikasrohra <vikasrohra2@gmail.com>
2024-04-13 15:21:38 +0530

    Added a class method

--- src/classes.ts
+++ src/classes.ts
@@ -1,10 +1,15 @@
 class Department { // As per the convension, first letter of class should be in caps
     name: string; // Property/field/variable, let/var/const is not required
-    constructor (n: string) { // For functions, no need to use const/let or function keywords
+
+    constructor (n: string) { // For constructor functions, no need to use const/let or function keywords
         this.name = n;
+    }
+
+    describe () { // For constructor functions, no need to use const/let or function keywords
+        console.log("Department: " + this.name);
     }
 }
 
 const accounting = new Department("Accounting");
 
-console.log(accounting);
+console.log(accounting.describe());
