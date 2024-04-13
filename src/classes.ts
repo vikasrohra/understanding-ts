a39d8980911f9eb2cef9b099bc85ab7207999f8b
vikasrohra <vikasrohra2@gmail.com>
2024-04-13 16:50:37 +0530

    readonly properties

--- src/classes.ts
+++ src/classes.ts
@@ -1,9 +1,9 @@
 class Department { // As per the convension, first letter of class should be in caps
-    // private id: string;
+    // private readonly id: string;
     // name: string; // Property/field/variable, let/var/const is not required
     private employees: string[] = [];
 
-    constructor (private id: string, public name: string) { // For constructor functions, no need to use const/let or function keywords
+    constructor (private readonly id: string, public name: string) { // For constructor functions, no need to use const/let or function keywords
         // this.id = id;
         // this.name = name;
     }
