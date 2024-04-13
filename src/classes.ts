e2745f99c7609f4f94b67084975f2b357c3aea9f
vikasrohra <vikasrohra2@gmail.com>
2024-04-13 16:31:14 +0530

    public and private access modifiers

--- src/classes.ts
+++ src/classes.ts
@@ -1,5 +1,6 @@
 class Department { // As per the convension, first letter of class should be in caps
-    name: string; // Property/field/variable, let/var/const is not required
+    private name: string; // Property/field/variable, let/var/const is not required
+    private employees: string[] = [];
 
     constructor (n: string) { // For constructor functions, no need to use const/let or function keywords
         this.name = n;
@@ -8,12 +9,28 @@ class Department { // As per the convension, first letter of class should be in 
     describe (this: Department) { // For constructor functions, no need to use const/let or function keywords
         console.log("Department: " + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
 
 accounting.describe();
 
-const accountingCopy = {name: "Dummy", describe: accounting.describe};
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 
-accountingCopy.describe();
+// accounting.employees[2] = "Anna"; // Not recommended, hence we should make class variables private so that they can be accessible only within the class
+
+accounting.printEmployeeInformation();
+
+// const accountingCopy = {name: "Dummy", describe: accounting.describe};
+
+// accountingCopy.describe();
