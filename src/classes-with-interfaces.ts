bceb792e3ef3a5ed9de2f2162a749e489bb6d438
vikasrohra <vikasrohra2@gmail.com>
2024-04-14 21:32:53 +0530

    Readonly in interfaces

--- src/classes-with-interfaces.ts
+++ src/classes-with-interfaces.ts
@@ -1,5 +1,5 @@
 interface Greetable {
-    name: string;
+    readonly name: string;
 
     greet(phrase: string): void;
 }
@@ -17,6 +17,9 @@ class Person implements Greetable {
     }
 }
 
-const user = new Person("Max");
+let user: Greetable;
+
+user = new Person("Max");
+// user.name = "Manu";
 
 user.greet("Hi there - I'm");
