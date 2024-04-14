da5d7da6d52e916df43ad6ca0c33c6ecbd81a4a6
vikasrohra <vikasrohra2@gmail.com>
2024-04-14 20:47:32 +0530

    Type in object

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,3 +16,23 @@ user1 = {
 }
 
 user1.greet("Hi there - I'm");
+
+
+type Person1 = {
+    name: string;
+    age: number;
+
+    greet(phrase: string): void;
+}
+
+let user2: Person1;
+
+user2 = {
+    name: 'Manu',
+    age: 29,
+    greet(phrase: string) {
+        console.log(phrase + " " + this.name);
+    }
+}
+
+user2.greet("Hi there - I'm");
