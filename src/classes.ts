e8f1a6ce407f4f86d2f35303ddd94d6c9404ddd8
vikasrohra <vikasrohra2@gmail.com>
2024-04-14 15:22:36 +0530

    Method Overriding and getters and setters

--- src/classes.ts
+++ src/classes.ts
@@ -1,7 +1,7 @@
 class Department { // As per the convension, first letter of class should be in caps
     // private readonly id: string;
     // name: string; // Property/field/variable, let/var/const is not required
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor (private readonly id: string, public name: string) { // For constructor functions, no need to use const/let or function keywords
         // this.id = id;
@@ -31,12 +31,39 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    // get propery, acts as an encapsulation 
+    get mostRecentReport() {
+        if(this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found.");
+    }
+
+    set mostRecentReport(value: string) {
+        if(!value) {
+            throw new Error("Please in a valid value!");
+        }
+        this.addReport(value);
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, "Accounting");
+        this.lastReport = reports[0];
+    }
+
+    addEmployee(employee: string) {
+        if(employee == "Max") {
+            return;
+        }
+
+        this.employees.push(employee);
     }
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     getReports() {
@@ -58,8 +85,12 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
-
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 accounting.getReports();
 
 // const itCopy = {name: "Dummy", describe: it.describe};
