8524e7fb602849a5ffd68b77051e90880f4c74f8
vikasrohra <vikasrohra2@gmail.com>
2024-04-16 21:28:46 +0530

    More useful decorator

--- src/class-decorator.ts
+++ src/class-decorator.ts
@@ -25,7 +25,22 @@ function Logger(logString: string) {
     }
 }
 
-@Logger('Logging - Person') // Here now we have to call the factory function
+function WithTemplate(template: string, hoodId: string) {
+    // return function(_: Function) { // Not interested in the constructor and to tell TypeScript that I'm not interested here, I can add an underscore as a name, which basically signals the TS, "Yeah I know I get this argument, but I don't need it"
+
+    return function(constructor: any) {
+        const hookEle = document.getElementById(hoodId);
+        const p = new constructor();
+        if(hookEle) {
+            hookEle.innerHTML = template;
+            hookEle.querySelector('h1')!.textContent = p.name;
+        }
+
+    }
+}
+
+// @Logger('Logging - Person') // Here now we have to call the factory function
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class NewPerson {
     name = 'Vikas';
 
