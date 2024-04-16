5654055d28dfede8e493ef38ab22025557be1662
vikasrohra <vikasrohra2@gmail.com>
2024-04-16 20:51:26 +0530

    Decorator Factories

--- src/class-decorator.ts
+++ src/class-decorator.ts
@@ -1,10 +1,31 @@
-// Decorator function, function name starts with capital letter, just for the convension
-function Logger(constructor: Function) {
-    console.log('Logging...');
-    console.log(constructor);
+// // Decorator function, function name starts with capital letter, just for the convension
+// function Logger(constructor: Function) {
+//     console.log('Logging...');
+//     console.log(constructor);
+// }
+
+// @Logger
+// class NewPerson {
+//     name = 'Vikas';
+
+//     constructor() {
+//         console.log('Creating person object...');
+//     }
+// }
+
+// const person = new NewPerson();
+
+// console.log(person);
+
+// Factory function that returns decorator function
+function Logger(logString: string) {    
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    }
 }
 
-@Logger
+@Logger('Logging - Person') // Here now we have to call the factory function
 class NewPerson {
     name = 'Vikas';
 
