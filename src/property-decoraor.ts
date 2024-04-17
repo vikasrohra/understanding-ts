a60a7752a21c76c00a71db693084f0b25c07ae98
vikasrohra <rohra-vikas@aramark.com>
2024-04-17 15:46:33 +0530

    Variable, property, method and parameter decorators

---
+++ src/property-decoraor.ts
@@ -0,0 +1,54 @@
+// Variable Decorator
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+// Property/Accessor Decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+// Method Decorator
+function Log3(target: any, name: string |Symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+// Method Parameter Decorator
+function Log4(target: any, name: string |Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position); // Index of the argument
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if(val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!')
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+}
